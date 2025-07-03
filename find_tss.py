aaa4a77c9d9f25a10d668a10dafdb66e99c45295
Cokol <cokol@bamgpu04.cm.cluster>
2025-07-03 15:42:33 -0400

    other gene models added to find_tss

--- find_tss.py
+++ find_tss.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
 genemodel = "MANE/1.3"
+# genemodel = "refSeq_v20240129"
+genemodel = "GENCODE/46/comprehensive/ALL"
+genemodel = "GENCODE/46/basic/PRI"
 genemodelpath = "hg38/gene_models/" + genemodel
 
 from dae.genomic_resources.gene_models import build_gene_models_from_resource_id
 GM = build_gene_models_from_resource_id(genemodelpath).load() 
 
 ### get all transcripts in gene model ###########################################################
-
 data = []
 for tm in GM.transcript_models.values():
     data.append({
@@ -22,15 +24,14 @@ df = pd.DataFrame(data)
 ### column pos has TSS
 df["pos"] = df.apply(lambda row: row["left"] if row["strand"] == "+" else row["right"], axis=1)
 df = df.drop(columns = ['left', 'right'])
-
 ### remove noncanonical chromosomes
 df = df[df["chrom"].str.len() <= 5]
+
 
 ### if a gene is repeated, then it has multiple TSS, drop them #################################
 gene_counts = df['gene'].value_counts()
 unique_genes = gene_counts[gene_counts == 1].index
 df = df[df['gene'].isin(unique_genes)]
-
 
 ### load the experimental RNA rpkms ############################################################
 rpkmdf = pd.read_csv("../input/median_rpkms.txt", sep="\t")
@@ -43,10 +44,9 @@ result = result.dropna()
 # Keep only rows where chrom_x equals chrom_y
 result = result[result["chrom_x"] == result["chrom_y"]]
 result = result.drop(columns=["chrom_y"]).rename(columns={"chrom_x": "chrom"})
+print(result.shape)
 
-print(result.head())
-
+### replace / so it isnt interpreted as directory
 genemodel = genemodel.replace("/", "_")
 outputpath = "../output/" + genemodel
 result.to_csv(outputpath, sep="\t", index=False)
-
